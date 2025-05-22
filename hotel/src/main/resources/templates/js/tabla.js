76f20922cb56ee2947264b55e95bbdfd2ae70beb
Adrián Collado Villalba <adrian20madrid@gmail.com>
2025-05-22 18:26:29 +0200

    Quitar contraseña en application.properties

---
+++ hotel/src/main/resources/templates/js/tabla.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $('#tabla-individual').DataTable({
+        paging: false,
+        searching: false,
+        info: false
+    });
+
+    $('#tabla-doble').DataTable({
+        paging: false,
+        searching: false,
+        info: false
+    });
+});
